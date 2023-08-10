4d263dcdd682c5304205b739898543c469c095fe
Godsfa <godsfavouromokhomion47@gmail.com>
2023-08-10 12:01:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Emtry point of the program
+ * main - prints out some string
  *
- * Return: Always 0 on success
+ * Return: 0 on success
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+puts("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
