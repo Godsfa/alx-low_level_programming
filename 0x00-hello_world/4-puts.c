987b3327699d4b0ae37b3b62665e3814b44a9d32
Godsfa <godsfavouromokhomion47@gmail.com>
2023-08-10 11:42:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
-int main(void) { puts("\"Programming is like building a multilingual puzzle"); return 0; }
+/**
+ * main - prints out some string
+ * Return: 0 on success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
