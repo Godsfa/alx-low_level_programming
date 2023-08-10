9291b1f394b4845fd225a6e6bc7ac7fa02cebdd3
Godsfa <godsfavouromokhomion47@gmail.com>
2023-08-10 06:32:40 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,2 @@
-#!/bin/bash
-echo '#include <stdio.h>
-int main() { puts("Programming is like building a multilingual puzzle,"); return 0; }' > puzzle.c
+#include <stdio.h>
+int main() { puts("Programming is like building a multilingual puzzle,"); return 0; }
