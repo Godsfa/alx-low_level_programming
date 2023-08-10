54c5ae29729051a9af6779dd0b641acf4432ce7e
Godsfa <godsfavouromokhomion47@gmail.com>
2023-08-10 07:13:16 -0700

    commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+/* Print the specified text using printf */
+printf("with proper grammar, but the outcome is a piece of art,\n");
+/* Return 0 to indicate successful execution */
+return (0);
+}
