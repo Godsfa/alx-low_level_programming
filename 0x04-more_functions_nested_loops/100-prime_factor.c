998c942c71b70e9b92ecfa421e5b2a913e66bc2d
Godsfa <godsfavouromokhomion47@gmail.com>
2023-08-17 13:41:28 -0700

    commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,8 +3,10 @@
 
 /**
  * largest_prime_factor - Finds the largest prime factor of a number
+ * main - Entry point of the program
  * @n: The number
- * Return: The largest prime factor
+ *
+ * Return: Always 0 (Success)
  */
 long largest_prime_factor(long n)
 {
