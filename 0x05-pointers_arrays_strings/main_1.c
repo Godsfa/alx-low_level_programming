49d279b2869b9e07d8ab0ac7bb5f705e64459e41
Godsfa <godsfavouromokhomion47@gmail.com>
2023-08-22 05:41:29 -0700

    commit

--- 0x05-pointers_arrays_strings/main_1.c
+++ 0x05-pointers_arrays_strings/main_1.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
-void puts2(char *);
+/**
+ * puts2 - Prints every other character of a string, starting from the first character.
+ * @str: The input string.
+ */
+void puts2(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        if (i % 2 == 0)
+        {
+            putchar(str[i]);
+        }
+        i++;
+    }
+    putchar('\n');
+}
 
 /**
  * main - check the code for Holberton School students.
@@ -9,9 +26,9 @@ void puts2(char *);
  */
 int main(void)
 {
-	char *str;
+    char *str;
 
-	str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\0Holberton";
-	puts2(str);
-	return (0);
+    str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\0Holberton";
+    puts2(str);
+    return (0);
 }
