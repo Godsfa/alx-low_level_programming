fabd356f1d86a4fd656d8fa50503e16e14634450
Godsfa <godsfavouromokhomion47@gmail.com>
2023-08-22 03:49:06 -0700

    commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,24 +5,22 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1; // To handle negative numbers
-    int result = 0;
-    int i = 0;
+	int sign = 1;
+	int result = 0;
+	int i = 0;
 
-    // Handle leading signs
-    while (s[i] == '-' || s[i] == '+')
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        i++;
-    }
+	while (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
 
-    // Convert the numeric characters to integer
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        result = result * 10 + (s[i] - '0');
-        i++;
-    }
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
 
-    return result * sign;
+	return (result * sign);
 }
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,13 +7,14 @@
  */
 int _strlen(char *s)
 {
-    int length = 0;
-    while (*s != '\0')
-    {
-        length++;
-        s++;
-    }
-    return length;
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
 }
 
 /**
@@ -22,14 +23,14 @@ int _strlen(char *s)
  */
 void rev_string(char *s)
 {
-    int length = _strlen(s);
-    int i, j;
-    char temp;
+	int length = _strlen(s);
+	int i, j;
+	char temp;
 
-    for (i = 0, j = length - 1; i < j; i++, j--)
-    {
-        temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
-    }
+	for (i = 0, j = length - 1; i < j; i++, j--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
 }
