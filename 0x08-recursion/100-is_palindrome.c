19da8f11e5b1dbc4c2ede4cd490f61e10644d295
Godsfa <godsfavouromokhomion47@gmail.com>
2023-08-30 18:58:53 +0100

    commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,75 +5,75 @@ int is_alpha_numeric(char c);
 char to_lower(char c);
 
 /**
- * is_palindrome - Checks if a string is a palindrome.
- * @s: The string to be checked.
- *
- * Return: 1 if the string is a palindrome, 0 otherwise.
- */
+* is_palindrome - Checks if a string is a palindrome.
+* @s: The string to be checked.
+*
+* Return: 1 if the string is a palindrome, 0 otherwise.
+*/
 int is_palindrome(char *s)
 {
-    int len = _strlen(s);
-    int i, j;
+int len = _strlen(s);
+int i, j;
 
-    if (len <= 1)
-        return 1;
-    i = 0;
-    j = len - 1;
+if (len <= 1)
+return (1);
+i = 0;
+j = len - 1;
 
-    while (i < j)
-    {
-        while (!is_alpha_numeric(s[i]))
-            i++;
-        while (!is_alpha_numeric(s[j]))
-            j--;
+while (i < j)
+{
+while (!is_alpha_numeric(s[i]))
+i++;
+while (!is_alpha_numeric(s[j]))
+j--;
 
-        if (i < j && to_lower(s[i]) != to_lower(s[j]))
-            return 0;
+if (i < j && to_lower(s[i]) != to_lower(s[j]))
+return (0);
 
-        i++;
-        j--;
-    }
+i++;
+j--;
+}
 
-    return 1;
+return (1);
 }
 
 /**
- * _strlen - Calculates the length of a string.
- * @s: The string.
- *
- * Return: The length of the string.
- */
+* _strlen - Calculates the length of a string.
+* @s: The string.
+*
+* Return: The length of the string.
+*/
 int _strlen(char *s)
 {
-    int len = 0;
-    while (*s)
-    {
-        len++;
-        s++;
-    }
-    return len;
+int len = 0;
+while (*s)
+{
+len++;
+s++;
+}
+return (len);
 }
 
 /**
- * is_alpha_numeric - Checks if a character is alphanumeric.
- * @c: The character to be checked.
- *
- * Return: 1 if the character is alphanumeric, 0 otherwise.
- */
+* is_alpha_numeric - Checks if a character is alphanumeric.
+* @c: The character to be checked.
+*
+* Return: 1 if the character is alphanumeric, 0 otherwise.
+*/
 int is_alpha_numeric(char c)
 {
-    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
 }
 
 /**
- * to_lower - Converts a character to lowercase.
- * @c: The character to be converted.
- *
- * Return: The lowercase character.
- */
+* to_lower - Converts a character to lowercase.
+* @c: The character to be converted.
+*
+* Return: The lowercase character.
+*/
 char to_lower(char c)
 {
-    if (c >= 'A' && c <= 'Z')
-        return c + 32;
-    return c;
+if (c >= 'A' && c <= 'Z')
+return (c + 32);
+return (c);
 }
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,44 +1,43 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- * sqrt_recursive_helper - Helper function for calculating the square root recursively
- * @n: The number for which to calculate the square root
- * @start: The starting point of the search range
- * @end: The ending point of the search range
- *
- * Return: The square root of n, or -1 if not found
- */
+* sqrt_recursive_helper - Helper function for calculating the square root recursively
+* @n: The number for which to calculate the square root
+* @start: The starting point of the search range
+* @end: The ending point of the search range
+*
+* Return: The square root of n, or -1 if not found
+*/
 int sqrt_recursive_helper(int n, int start, int end)
 {
-    int mid;
+int mid;
 
-    if (start <= end)
-    {
-        mid = start + (end - start) / 2;
+if (start <= end)
+{
+mid = start + (end - start) / 2;
 
-        if (mid * mid == n)
-            return mid;
+if (mid * mid == n)
+return (mid);
 
-        if (mid * mid > n)
-            return sqrt_recursive_helper(n, start, mid - 1);
+if (mid * mid > n)
+return (sqrt_recursive_helper(n, start, mid - 1));
 
-        return sqrt_recursive_helper(n, mid + 1, end);
-    }
+return (sqrt_recursive_helper(n, mid + 1, end));
+}
 
-    return -1;
+return (-1);
 }
 
 /**
- * _sqrt_recursion - Returns the natural square root of a number
- * @n: The number for which to calculate the square root
- *
- * Return: The square root of n, or -1 if not found
- */
+* _sqrt_recursion - Returns the natural square root of a number
+* @n: The number for which to calculate the square root
+*
+* Return: The square root of n, or -1 if not found
+*/
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return -1;
+if (n < 0)
+return (-1);
 
-    return sqrt_recursive_helper(n, 0, n);
+return (sqrt_recursive_helper(n, 0, n));
 }
