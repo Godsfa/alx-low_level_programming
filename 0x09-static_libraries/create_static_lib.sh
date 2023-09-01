bc5a66102272e2dc6629ffc4b31264574c8856fc
Godsfa <godsfavouromokhomion47@gmail.com>
2023-09-01 15:11:52 +0100

    commiting all files in task one

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Compile all .c files in the current directory into .o files
-gcc -Wall -pedantic -Werror -Wextra -c *.c
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c *.c
 
-# Create the static library liball.a from the .o files
-ar rc liball.a *.o
+ar -rc liball.a *.o
 
-# Index the library
 ranlib liball.a
 
-# Clean up the .o files
 rm *.o
