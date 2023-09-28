55dd323b5e00dd109a4aa11ebc5015daf079ca81
Godsfa <godsfavouromokhomion47@gmail.com>
2023-09-28 15:01:53 +0100

    commiting cause of betty

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,21 +1,21 @@
-#include "main.h"  /* Include the header file where unsigned long int is defined */
+#include "main.h"
 
 /**
- * get_bit - Returns the value of a bit at a given index.
- * @n: The number to extract the bit from.
- * @index: The index of the bit to get, starting from 0.
- *
- * Return: The value of the bit at index, or -1 if an error occurred.
- */
+* get_bit - Returns the value of a bit at a given index.
+* @n: The number to extract the bit from.
+* @index: The index of the bit to get, starting from 0.
+*
+* Return: The value of the bit at index, or -1 if an error occurred.
+*/
 int get_bit(unsigned long int n, unsigned int index)
 {
-    int bit_value;
+int bit_value;
 
-    if (index >= sizeof(unsigned long int) * 8)
-        return (-1); /* Error: Index out of range */
+if (index >= sizeof(unsigned long int) * 8)
+return (-1); /* Error: Index out of range */
 
-    /* Use bitwise shift and masking to get the value of the bit at index */
-    bit_value = (n >> index) & 1;
+/* Use bitwise shift and masking to get the value of the bit at index */
+bit_value = (n >> index) & 1;
 
-    return (bit_value);
+return (bit_value);
 }
